6c946287591b0083a3f742c455da62927836504f
exam <exam@Bcalab4-m01>
2023-05-04 12:13:30 +0530

    this is a Branch commit

--- src/Square_223.java
+++ src/Square_223.java
@@ -1,5 +1,13 @@
 
 public class Square_223 {
+	int length;
+	int area;
+	int perimeter;
+	 Square_223(int length)
+	 {
+		 this.length=length;
+	 }
+	 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
